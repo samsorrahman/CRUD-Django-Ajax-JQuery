0bd70c8d3aafad00df3444c9fc82c0f279293506
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-20 23:30:30 +0500

    save data completed

--- djangoajaxcrud/enroll/views.py
+++ djangoajaxcrud/enroll/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
 from .forms import StudentRegistration
 from .models import User
-# Createyour views here.
+from django.http import JsonResponse
+# from django.views.decorators.csrf import csrf_exempt
+# Create your views here.
 
 
 def home(request):
     form = StudentRegistration()
     stud = User.objects.all()
     return render(request, 'enroll/home.html', {'form': form, 'stu': stud})
+
+# @csrf_exempt
+
+
+def save_data(request):
+    if request.method == "POST":
+        form = StudentRegistration(request.POST)
+        if form.is_valid():
+            sid = request.POST.get('stuid')
+            name = request.POST['name']
+            email = request.POST['email']
+            password = request.POST['password']
+            if (sid == ''):
+                usr = User(name=name, email=email, password=password)
+            else:
+                usr = User(id=sid, name=name, email=email, password=password)
+
+            usr.save()
+            stud = User.objects.values()
+            student_data = list(stud)
+            return JsonResponse({'status': 'Save', 'student_data': student_data})
+        else:
+            return JsonResponse({'status': 0})
